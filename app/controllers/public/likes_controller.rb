06c69a5a4152976e9d1c38529750d1257d60eb64
minatubouti <pingneimeinai@gmail.com>
2023-08-14 14:24:39 +0000

    退会済ユーザーの表示制限、ヘルパーメソッド使用

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,13 +1,13 @@
 class Public::LikesController < ApplicationController
  
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
-    @post = @like.post
-    respond_to do |format|
-     format.js
-    end
+   @like = current_user.likes.create(post_id: params[:post_id])
+   @post = @like.post
+   respond_to do |format|
+    format.js
+   end
   end
-    
+
   def destroy
     @like = Like.find(params[:id])
     @post = @like.post
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+   def active_likes_count(post)
+    post.likes.joins(:user).where(users: { is_deleted: false }).count
+  end
+
+  def active_bookmarks_count(post)
+    post.bookmarks.joins(:user).where(users: { is_deleted: false }).count
+  end
+
+  def active_comments_count(post)
+    post.comments.joins(:user).where(users: { is_deleted: false }).count
+  end
+
 end
