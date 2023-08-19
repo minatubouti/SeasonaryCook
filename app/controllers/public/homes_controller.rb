dbaa8d2763e3590ea5e677170174706c27ac55b5
minatubouti <pingneimeinai@gmail.com>
2023-08-19 05:05:55 +0000

    homes_controller

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     # 最新の投稿から5つの画像をランダムに取得
-     @images = Post.order("RANDOM()").limit(5).map { |post| url_for(post.image) }
+     @images = Post.order(Arel.sql('RANDOM()')).limit(5).map { |post| url_for(post.image) }
   end
 
   def about
