66a8ccbfa847ef904c29bfd42bc615be029270f8
minatubouti <pingneimeinai@gmail.com>
2023-08-19 04:43:02 +0000

    tagマイグレーション

--- db/migrate/20230816084509_add_missing_taggable_index.acts_as_taggable_on_engine.rb
+++ db/migrate/20230816084509_add_missing_taggable_index.acts_as_taggable_on_engine.rb
@@ -3,11 +3,11 @@
 
 class AddMissingTaggableIndex < ActiveRecord::Migration[6.0]
   def self.up
-    add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type context],
-              name: 'taggings_taggable_context_idx'
+    # add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type context],
+    #           name: 'taggings_taggable_context_idx'
   end
 
   def self.down
-    remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
+    # remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
   end
 end
