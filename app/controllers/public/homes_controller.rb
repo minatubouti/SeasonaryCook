65d60fca4a0e014ae601aa215785e589f7e36e64
minatubouti <pingneimeinai@gmail.com>
2023-08-19 10:51:29 +0000

    投稿一覧-レイアウト修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
-    # 最新の投稿から5つの画像をランダムに取得
+    # 本番環境(mySQL)とSQLliteで関数を使い分ける
      rand = Rails.env.production? ? "rand()" : "RANDOM()"
+     # 最新の投稿から5つの画像をランダムに取得
      @images = Post.order(rand).limit(5).map { |post| url_for(post.image) }
   end
 
