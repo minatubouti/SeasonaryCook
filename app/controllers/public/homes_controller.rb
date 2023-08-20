6cfc25dd38cba2e6a41bf8403c0b66bf964700d3
minatubouti <pingneimeinai@gmail.com>
2023-08-20 01:18:16 +0000

    調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,13 @@
 class Public::HomesController < ApplicationController
   def top
-    # 本番環境(mySQL)とSQLliteで関数を使い分ける
-     rand = Rails.env.production? ? "rand()" : "RANDOM()"
-     # 最新の投稿から5つの画像をランダムに取得
-     @images = Post.order(rand).limit(5).map { |post| url_for(post.image) }
+      # 最新の投稿から5つを取得
+    @posts = Post.order(created_at: :desc).limit(5)
+    # RubyのArray#shuffleメソッドでランダムに並び替え
+    @images = @posts.shuffle.map { |post| url_for(post.image) }
+    # # 本番環境(mySQL)とSQLliteで関数を使い分ける
+    # rand = Rails.env.production? ? "rand()" : "RANDOM()"
+    # # 最新の投稿から5つの画像をランダムに取得
+    # @images = Post.order(rand).limit(5).map { |post| url_for(post.image) }
   end
 
   def about
