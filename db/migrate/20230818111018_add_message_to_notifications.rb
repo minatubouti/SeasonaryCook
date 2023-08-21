7e7b6bb26699472d42ac26de7ca4f1ad9b11bd38
minatubouti <pingneimeinai@gmail.com>
2023-08-21 06:48:34 +0000

    マイグレーションファイル削除

--- db/migrate/20230818111018_add_message_to_notifications.rb
+++
@@ -1,5 +0,0 @@
-class AddMessageToNotifications < ActiveRecord::Migration[6.1]
-  def change
-    add_column :notifications, :message, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 2023_08_19_112807) do
     t.integer "post_id"
     t.integer "comment_id"
     t.integer "inquiry_id"
-    t.text "message"
     t.string "action", default: "", null: false
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
