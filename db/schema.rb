44cd72bfcd38766556e59c2244de1f458389739f
minatubouti <pingneimeinai@gmail.com>
2023-08-21 15:03:48 +0000

    テストコード導入

--- db/schema.rb
+++ db/schema.rb
@@ -155,7 +155,6 @@ ActiveRecord::Schema.define(version: 2023_08_21_090716) do
     t.datetime "created_at"
     t.string "tenant", limit: 128
     t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
     t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
     t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
@@ -173,7 +172,6 @@ ActiveRecord::Schema.define(version: 2023_08_21_090716) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create!(
+  [
+    {email: 'olivia@example.com', name: 'Olivia', password: 'password'},
+    {email: 'james@example.com', name: 'James', password: 'password'},
+    {email: 'lucas@example.com', name: 'Lucas', password: 'password'}
+  ]
+)
+
+users = User.all
+
+posts = [
+  { title: '蒸し野菜', main_vegetable: 'ブロッコリー', season: '春', is_public: true, description: 'ヘルシーで栄養たっぷり！', user_id: users[0].id },
+  { title: '野菜たっぷりポトフ', main_vegetable: 'キャベツ', season: '冬', is_public: true, description: '体の芯から温まる', user_id: users[1].id },
+  { title: '蓮根と夏野菜あんかけ', main_vegetable: '蓮根', season: '夏', is_public: true, description: '野菜たっぷりで美味しい！', user_id: users[2].id }
+]
+
+images = [
+  "#{Rails.root}/db/fixtures/sample-post1.jpg",
+  "#{Rails.root}/db/fixtures/sample-post2.jpg",
+  "#{Rails.root}/db/fixtures/sample-post3.png"
+]
+
+posts.each_with_index do |post_data, index|
+  post = Post.create!(post_data)
+  image_blob = ActiveStorage::Blob.create_and_upload!(io: File.open(images[index]), filename: File.basename(images[index]))
+  post.image.attach(image_blob)
+end
