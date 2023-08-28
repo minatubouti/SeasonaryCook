0d3a1cb46ca70146e3de42cef5583c856e0b1fff
minatubouti <pingneimeinai@gmail.com>
2023-08-28 15:05:07 +0000

    自身の投稿の場合いいね、コメント通知を作成しないように

--- app/models/post.rb
+++ app/models/post.rb
@@ -55,12 +55,16 @@ class Post < ApplicationRecord
     bookmarks.exists?(user_id: user.id)
   end
   
-# いいね通知の作成メソッド
+  
+  # いいね通知の作成メソッド
   def create_notification_like!(current_user)
+    user_id = self.user_id  # self はこの場合 Post オブジェクトを指す
+    
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
-    # いいねされていない場合のみ、通知レコードを作成
-    if temp.blank?
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
+  
+    # いいねされていない場合、かつ、自分自身へのいいねでない場合のみ、通知レコードを作成
+    if temp.blank? && current_user.id != user_id
       notification = current_user.active_notifications.new(
         post_id: id,
         visited_id: user_id,
@@ -70,35 +74,36 @@ class Post < ApplicationRecord
       if notification.visitor_id == notification.visited_id
         notification.checked = true
       end
+  
       notification.save if notification.valid?
     end
   end
-  
+
   
 # コメント通知の作成メソッド
-    def create_notification_comment!(current_user, comment_id)
-    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-      temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
-      temp_ids.each do |temp_id|
-        save_notification_comment!(current_user, comment_id, temp_id['user_id'])
-      end
-      # まだ誰もコメントしていない場合は、投稿者に通知を送る
-      save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  def create_notification_comment!(current_user, comment_id)
+  # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
+    temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      # 自分自身の投稿へのコメントでなければ通知を保存
+      save_notification_comment!(current_user, comment_id, temp_id['user_id']) if current_user.id != temp_id['user_id']
     end
-  
-    def save_notification_comment!(current_user, comment_id, visited_id)
-      # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
-      notification = current_user.active_notifications.new(
-        post_id: id,
-        comment_id: comment_id,
-        visited_id: visited_id,
-        action: 'comment'
-      )
-      # 自分の投稿に対するコメントの場合は、通知済みとする
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
-      notification.save if notification.valid?
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る（自分自身でなければ）
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank? && current_user.id != user_id
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
     end
-    
+    notification.save if notification.valid?
+  end
 end
