20e9c1f9e6616ac6e1f34f0bfb72c382af4d5539
minatubouti <pingneimeinai@gmail.com>
2023-09-10 02:55:31 +0000

    タグ検索/投稿の表示制限

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -1,15 +1,21 @@
 class Public::TagsController < ApplicationController
   def index
+    # タグのキーワード検索が行われた場合
     if params[:search].present?
       @tags = ActsAsTaggableOn::Tag.where('name LIKE ?', "%#{params[:search]}%")
     else
-      # 使用されている上位25個のタグを取得
+     # タグの検索が行われなかった場合、最も多く使用された上位30のタグを表示
       @tags = ActsAsTaggableOn::Tag.most_used(30)
     end
   end
   
+  # 特定のタグに関連する投稿一覧を表示
   def show
+    # 選択されたタグを取得
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    # 該当するタグが付与された投稿を取得し、特定の条件（非公開、ゲスト、退会済みのユーザーの投稿を除外）で絞り込む
     @posts = Post.tagged_with(@tag.name)
+                .where(is_deleted: false, is_guest: false) # 退会済み、ゲストユーザーの投稿を除外
+                .where(is_public: true) # 公開投稿のみを取得
   end
 end
