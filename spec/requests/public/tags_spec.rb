3fc46c11287a226681f101ee0688efa87f3c0304
minatubouti <pingneimeinai@gmail.com>
2023-09-15 06:56:23 +0000

    rspecテストエラー修正

--- spec/requests/public/tags_spec.rb
+++ spec/requests/public/tags_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Public::Tags", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/public/tags/index"
+      get "/tags"
       expect(response).to have_http_status(:success)
     end
   end
