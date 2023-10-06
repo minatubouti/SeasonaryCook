2d09c9d983531cd277de5f6da225d8159e148f70
minatubouti <pingneimeinai@gmail.com>
2023-10-06 14:58:42 +0000

    orderテーブル作成

---
+++ db/migrate/20231006135451_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.integer :shop_id
+      t.integer :total_price
+      t.string :status
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_04_051657) do
+ActiveRecord::Schema.define(version: 2023_10_06_135451) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -115,6 +115,15 @@ ActiveRecord::Schema.define(version: 2023_10_04_051657) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "data"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "shop_id"
+    t.integer "total_price"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
