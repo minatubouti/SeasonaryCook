d4139d82dcee5f30507ada874b0fa74876bd6f8c
minatubouti <pingneimeinai@gmail.com>
2023-11-05 05:29:50 +0000

    addressesコントローラー作成

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,48 @@
+class Public::AddressesController < ApplicationController
+  before_action :authenticate_user!
+  
+    def index
+      @addresses = current_user.addresses
+      @address = Address.new
+    end
+
+    def edit
+      @address = Address.find(params[:id])
+    end
+  
+    def create
+      @address = Address.new(address_params)
+      @address.user_id = current_user.id
+      if  @address.save
+        flash[:notice] = "配送先の登録に成功しました"
+        redirect_to addresses_path
+      else
+        @addresses = current_user.addresses
+        redirect_to addresses_path
+      end
+    end
+
+    def update
+      @address = Address.find(params[:id])
+      if @address.update(address_params)
+        flash[:notice] = "配送先の更新に成功しました"
+        redirect_to addresses_path
+      else
+        redirect_to edit_address_path(@address)
+      end
+    end
+  
+    def destroy
+      @address = Address.find(params[:id])
+      @address.destroy
+      @address = current_user.addresses
+       flash[:notice] = "登録内容は正常に削除されました"
+       redirect_to addresses_path
+    end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postcode, :name, :address).merge(user_id: current_user.id)
+  end
+end
