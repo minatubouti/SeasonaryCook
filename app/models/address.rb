81ede01710a824a90ad63122be559dd51f289786
minatubouti <pingneimeinai@gmail.com>
2023-11-07 06:33:06 +0000

    アドレス登録ページ作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+  belongs_to :user
+
+   def address_display
+     '〒' + postcode + ' ' + address + ' ' + name
+   end
+
+   validates :postcode, presence:true,length: { is: 7 }
+   validates :address, presence:true
+   validates :name, presence:true
+
 end
