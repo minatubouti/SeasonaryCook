539422d7e210c55ec8cfae196e5c610cba958e3b
minatubouti <pingneimeinai@gmail.com>
2023-11-20 02:43:48 +0000

    調整

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ApplicationRecord
   belongs_to :address
   has_many :order_items
   enum payments: { credit_card: 0, bank_transfer: 1 }
+  
+  def total_price
+    order_items.sum { |item| item.quantity * item.unit_price }
+  end
 end
